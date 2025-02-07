7fbc2f9f5cfb26f776c249447e52599bc2f31f0a
Power70 <nwisuanu@gmail.com>
2025-02-07 22:21:42 +0100

    learn about routing module

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ////////////////////////////////////////////////////////////////
 // FILE SYSTEM
@@ -20,23 +21,45 @@ const http = require("http");
 // console.log(hello);
 
 // Doing everything in an asynchronous manner
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
-        console.log(data2);
-        fs.readFile('txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
-            // Read data2 and data3 from the file above andd write them to a test file
-            fs.writeFile('txt/test.txt',`${data2}\n${data3}`, 'utf-8', err =>{
-                console.log('Your file was successfully written.')
-            });
-        });
-    });
-    // console.log(data);
-    // Node sees a callback function as an async function
-});
-console.log('Hello from the main thread');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+//         console.log(data2);
+//         fs.readFile('txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             // Read data2 and data3 from the file above andd write them to a test file
+//             fs.writeFile('txt/test.txt',`${data2}\n${data3}`, 'utf-8', err =>{
+//                 console.log('Your file was successfully written.')
+//             });
+//         });
+//     });
+//     // console.log(data);
+//     // Node sees a callback function as an async function
+// });
+// console.log('Hello from the main thread');
 
 ////////////////////////////////////////////////////////////////
 // SERVER
 
 // In order to use the srver we have to do 2 things: First we create a server and second we start the server
+
+// 1. Create a server: It accepte a callback function with two parameters request and response.
+const server = http.createServer((req, res) =>{
+    // res.end('Hello from the server')
+    const pathName = req.url;
+    if (pathName == '/'|| pathName == '/overview'){
+        res.end('Overview Page');
+    }else if(pathName == '/product'){
+        res.end('Product Page');
+    }
+    else{
+        res.writeHead(400, {
+            'content-type': 'text/html',
+            'my-own-header': 'Hello from the server'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
+// 2. listen on the server
+server.listen(8000, 'localhost', () =>{
+    console.log('Server is running on http://localhost:8000');
+});
